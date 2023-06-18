96d6a466bfd04a7de14f63a5d79b012eb087d0fb
Thewolfinsociety <15179462795@163.com>
2023-06-18 09:18:24 +0800

    插入数据改为文本提交

--- src/codevecdb/search_code.py
+++ src/codevecdb/search_code.py
@@ -3,13 +3,14 @@ from src.codevecdb import langchianEmbedding
 from src.codevecdb.milvus_vectordb import searchVectorCode, searchRecentData
 from src.codevecdb.llm import getAnswer
 
-def searchCode(query, top_k=5):
-    semantics_list = [query]
-    print(semantics_list)
+def searchCode(question, top_k=5):
+    semantics_list = [question]
     codeVectorList = langchianEmbedding.get_semantics_vector(semantics_list)
     dataBaseData = searchVectorCode(codeVectorList, top_k)
-    answer = getAnswer(dataBaseData, query)
-    return {"answer":answer}
+    answer = getAnswer(dataBaseData, question)
+    result = {"question":question, "answer":answer}
+    print(result)
+    return result
 
 
 def getAllCode(top_k=100):
